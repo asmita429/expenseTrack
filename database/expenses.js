90d3fb1952826ff502594064e1b54b93d36a6f06
asmita <asmitathapa429@gmail.com>
2023-07-07 15:04:37 +0545

    updated expense track after file

---
+++ database/expenses.js
@@ -0,0 +1,25 @@
+let expenses = [
+    {
+        "id" : 1,
+        "title" : "food",
+        "paidBy" : "Sonika",
+        "description": "Lunch in a restaurent",
+        "ammount" : 2000
+    },
+    {
+        "id" : 2,
+        "title" : "transport",
+        "paidBy" : "Aayesha",
+        "description": "Commute fare from Butwal to Lunbini",
+        "ammount" : 1000
+    },
+    {
+        "id" : 3,
+        "title" : "hotel",
+        "paidBy" : "Suvu",
+        "description": "Hotel Dreamland fare",
+        "ammount" : 10000
+    }
+]
+
+module.exports = expenses;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var expenses = require('../database/expenses');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(expenses);
+  res.render('index', { title: 'expense tracker', expenseList: expenses });
+
 });
 
 
@@ -13,6 +17,46 @@ router.get('/wlit', function(req, res, next) {
 
 router.get('/assignment', function(req, res, next) {
   res.render('index', { title: 'WLiT', number: 10 });
+});
+
+router.get('/add',function(req, res, next) {
+  res.render('addExpense');
+});
+
+router.post('/saveexpense', function(req,res,next){
+  let formData = {
+    "title" : req.body.title, //form title name ='title'
+    "paidBy" : req.body.paidBy, //form paidBy name ='paidBy'
+    "descriotion" : req.body.descriotion,
+    "amount" : req.body.amount
+  }
+  console.log(formData);
+  expenses.push({...formData, id:expenses.length+1})
+  res.redirect('/');
+});
+
+router.get('/delete/:index', function(req,res,next){
+  expenses.splice(req.params.index, 1);
+  res.redirect('/');
+});
+
+router.get('/edit/:id', function(req,res, next){
+  const expense = expenses.find(expense => expense.id == req.params.id)
+
+  res.render('editExpense',{expense:expense})
+});
+
+router.post('/saveEdited/:id',function(req,res, next){
+    var formData = {
+      "title" : req.body.title, //form title name ='title'
+      "paidBy" : req.body.paidBy, //form paidBy name ='paidBy'
+      "descriotion" : req.body.descriotion,
+      "amount" : req.body.amount
+    }
+    const index = expenses.findIndex(expenses => {
+    return expenses.id == req.params.id});
+    expenses.splice(index, 1, {id:req.params.id, ...formData})
+    res.redirect('/');
 });
 
 module.exports = router;
